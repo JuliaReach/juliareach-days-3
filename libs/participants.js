09743ce1464913f8440622707a338a56922b77d0
mforets <mforets@gmail.com>
2021-11-10 00:13:48 +0000

    Deploying to gh-pages from  @ cb0394613e26934bf43fc9d46c32ccabec33714e 🚀

---
+++ libs/participants.js
@@ -0,0 +1,49 @@
+const persons = [
+    {
+        name : 'Marcelo Forets',
+        lat : -34.8813764,
+        lon : -55.051468,
+        github : 'mforets',
+        linkedin: 'https://www.linkedin.com/in/marcelo-forets-irurtia-7b8238149/',
+    },
+    {
+        name : 'Jorge Pérez Zerpa',
+        lat : -34.8813764,
+        lon : -55.051468,
+        github : 'jorgepz',
+        linkedin: 'https://uy.linkedin.com/in/jorgejmpz',
+        homepage: 'https://www.fing.edu.uy/~jorgepz/'
+    },
+    {
+        name : 'Luca Ferranti',
+        lat : 60.186836,
+        lon : 24.821351,
+        github : 'lucaferranti',
+        linkedin: 'https://www.linkedin.com/in/luca-ferranti/',
+        homepage: 'https://lucaferranti.github.io'
+    },
+    {
+        name: 'Christian Schilling',
+        lat: 57.015,
+        lon: 9.985,
+        github: 'schillic',
+        homepage: 'https://www.christianschilling.net/'
+    },
+    {
+        name: 'Luis Benet',
+        lat: 18.9820975,
+        lon: -99.2372167,
+        github: 'lbenet',
+        twitter: 'luisbenetf',
+        homepage: 'https://lbenet.github.io/'
+    },
+    {
+        name: 'David Sanders',
+        lat: 42.361145,
+        lon: -71.057083,
+        github: 'dpsanders',
+        twitter: 'DavidPSanders',
+        linkedin: 'https://www.linkedin.com/in/david-p-sanders-05aa246/',
+        homepage: 'https://sistemas.fciencias.unam.mx/~dsanders/'
+    }
+];
