b6f45a0fcb4b4f445b068eb31ee5b95404733f2d
mforets <mforets@gmail.com>
2021-11-12 18:10:18 +0000

    Deploying to gh-pages from  @ 04e918c6f4d8a8fdb550a374c5e3889d0e3a2b0f 🚀

--- data/speakers.jl
+++ data/speakers.jl
@@ -9,7 +9,7 @@ speakers = [
     Speaker("Marcelo Forets", "forets.png", "Universidad de la República, Uruguay", "https://mforets.github.io/"),
     Speaker("Jorge Pérez Zerpa", "Jorge_Perez.png", "Universidad de la República, Uruguay", "https://www.fing.edu.uy/~jorgepz/"),
     Speaker("Luca Ferranti", "ferranti.jpeg", "University of Vaasa, Finland", "https://lucaferranti.github.io"),
-    Speaker("Christian Schilling", "schilling.jfif", "Aalborg University, Denmark", "https://www.christianschilling.net/"),
+    Speaker("Miriam García Soto", "ist.jpg", "IST Austria, Austria", "http://pub.ist.ac.at/~mgarcias/"),
     Speaker("Arvind Adimoolam", "Arvind.jpg", "Indian Institute of Technology (IIT) Kanpur, India", "https://www.linkedin.com/in/arvind-adimoolam-425a1932/"),
     Speaker("Paulius Stankaitis", "Paulius_Stankaitis.jpg", "Newcastle University, United Kingdom", "https://dblp.org/pid/174/1117.html"),
     Speaker("Roberto Rocchetta", "Roberto.jpg", "Technical University of Eindhoven, Netherlands", "https://www.linkedin.com/in/roberto-rocchetta-737b857a/"),
